015e77f2a5afa6e1a23c8f163af2fba60f4e4738
frans nekongo <FRANSINATOR2002@GMAIL.COM>
2024-07-22 22:31:04 +0200

    created card view,

--- learnersquizna/components/Questioncard.tsx
+++ learnersquizna/components/Questioncard.tsx
@@ -1,94 +1,41 @@
 import React from "react";
 import {Card, CardFooter, Image, Button} from "@nextui-org/react";
-import {CardHeader} from "@nextui-org/card";
+import {CardBody, CardHeader} from "@nextui-org/card";
+import {Radio, RadioGroup} from "@nextui-org/radio";
 
-export  function Questioncard() {
+export function Questioncard() {
     return (
-        <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-            <Card className="col-span-12 sm:col-span-4 h-[300px]">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
-                    <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
+        // grid-rows-3
+        <div className="grid grid-flow-row-dense grid-cols-2  gap-4">
+            <Card className="py-4 bg-white/5">
+                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                    <p className="text-tiny font-semibold">52. A section of the road is marked with double unbroken
+                        dividing
+                        lines as in the diagram. When is
+                        a vehicle allowed to cross the lines?
+                    </p>
                 </CardHeader>
-                <Image
-                    removeWrapper
-                    alt="Card background"
-                    className="z-0 w-full h-full object-cover"
-                    src="https://nextui.org/images/card-example-4.jpeg"
-                />
-            </Card>
-            <Card className="col-span-12 sm:col-span-4 h-[300px]">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-                    <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
+                <CardBody className="overflow-visible py-2">
+                    <Image
+                        alt="Card background"
+                        className="object-cover rounded-xl"
+                        src="https://isqkzbwoiunnqsltbfpa.supabase.co/storage/v1/object/public/question_images/test%20card%201.png?t=2024-07-22T15%3A35%3A53.713Z"
+                        width={270}
+                    />
+                </CardBody>
+                <CardHeader>
+                    <RadioGroup color="warning">
+                        <Radio value="buenos-aires" description="The capital of Argentina">
+                            A
+                        </Radio>
+                        <Radio value="canberra" description="The capital of Australia">
+                            B
+                        </Radio>
+                        <Radio value="london" description="The capital of England">
+                            C
+                        </Radio>
+                    </RadioGroup>
                 </CardHeader>
-                <Image
-                    removeWrapper
-                    alt="Card background"
-                    className="z-0 w-full h-full object-cover"
-                    src="https://nextui.org/images/card-example-3.jpeg"
-                />
-            </Card>
-            <Card className="col-span-12 sm:col-span-4 h-[300px]">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
-                    <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
-                </CardHeader>
-                <Image
-                    removeWrapper
-                    alt="Card background"
-                    className="z-0 w-full h-full object-cover"
-                    src="https://nextui.org/images/card-example-2.jpeg"
-                />
-            </Card>
-            <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-                <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-                    <h4 className="text-black font-medium text-2xl">Acme camera</h4>
-                </CardHeader>
-                <Image
-                    removeWrapper
-                    alt="Card example background"
-                    className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-                    src="https://nextui.org/images/card-example-6.jpeg"
-                />
-                <CardFooter
-                    className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-                    <div>
-                        <p className="text-black text-tiny">Available soon.</p>
-                        <p className="text-black text-tiny">Get notified.</p>
-                    </div>
-                    <Button className="text-tiny" color="primary" radius="full" size="sm">
-                        Notify Me
-                    </Button>
-                </CardFooter>
-            </Card>
-            <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-                <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
-                    <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
-                </CardHeader>
-                <Image
-                    removeWrapper
-                    alt="Relaxing app background"
-                    className="z-0 w-full h-full object-cover"
-                    src="https://nextui.org/images/card-example-5.jpeg"
-                />
-                <CardFooter
-                    className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-                    <div className="flex flex-grow gap-2 items-center">
-                        <Image
-                            alt="Breathing app icon"
-                            className="rounded-full w-10 h-11 bg-black"
-                            src="https://nextui.org/images/breathing-app-icon.jpeg"
-                        />
-                        <div className="flex flex-col">
-                            <p className="text-tiny text-white/60">Breathing App</p>
-                            <p className="text-tiny text-white/60">Get a good night's sleep.</p>
-                        </div>
-                    </div>
-                    <Button radius="full" size="sm">Get App</Button>
-                </CardFooter>
             </Card>
         </div>
     );
