ac79f2288357cac98b10b274ee9be6bcb8efaf35
frans nekongo <FRANSINATOR2002@GMAIL.COM>
2024-08-31 01:02:18 +0200

    SEO integration phase 1

--- learnersquizna/app/layout.tsx
+++ learnersquizna/app/layout.tsx
@@ -3,20 +3,18 @@ import "./globals.css";
 import React from "react";
 
 import {Providers} from "@/components/NextUI/Providers";
+import {Metadata} from "next";
+import {customMetaDataGenerator} from "@/lib/customMetaDataGenerator";
 
 const defaultUrl = "https://namibianlearnerstest.frans-nekongo.com/"
     ? `https://namibianlearnerstest.frans-nekongo.com/`
     : "http://localhost:3000";
 
 const url = "https://isqkzbwoiunnqsltbfpa.supabase.co/storage/v1/object/public/WebsiteLogo/logo.ico?t=2024-07-25T21%3A32%3A36.405Z";
-export const metadata = {
-    metadataBase: new URL(defaultUrl),
+
+export const metadata: Metadata = customMetaDataGenerator({
     title: 'Namibian Learners licence Test',
-    description: 'A quiz to help practice for your Namibian Learners licence test',
-    icons: {
-        icon: url
-    },
-};
+});
 
 export default function RootLayout({
                                        children,
---
+++ learnersquizna/lib/customMetaDataGenerator.ts
@@ -0,0 +1,96 @@
+// File location: @/lib/customMetaDataGenerator.ts
+import {Metadata} from 'next';
+
+interface PageSEOProps {
+    title: string;
+    description?: string;
+    canonicalUrl?: string;
+    ogType?: 'website' | 'article' | 'book' | 'profile' | 'music.song' | 'music.album' | 'music.playlist' | 'music.radio_station' | 'video.movie' | 'video.episode' | 'video.tv_show' | 'video.other';
+    ogImage?: string;
+    twitterCard?: 'summary_large_image' | 'summary' | 'player' | 'app'; // Restricting to specific types
+    keywords?: string[];
+}
+
+export function customMetaDataGenerator({
+                                            title,
+                                            description = "Easily pass your Namibian Learners Licence test online! and improve your chances of passing on the first attempt.",
+                                            canonicalUrl = 'https://namibianlearnerstest.frans-nekongo.com/',
+                                            ogType = 'website',
+                                            keywords = [
+                                                "Namibian learners licence test",
+                                                "NaTIS Questions and Answers",
+                                                "Namibia NaTIS test preparation",
+                                                "learners licence test Namibia",
+                                                "online Namibian driving test",
+                                                "Namibia road safety",
+                                                "NaTIS exam tips",
+                                                "learners licence practice Namibia",
+                                                "Namibian driving licence preparation",
+                                                "Namibian learners permit test",
+                                                "Namibian driving school resources",
+                                                "NaTIS Questions and Answers",
+                                                "Elidge Namibian Learners",
+                                                "Elidge",
+                                                "Namibian learners licence test online",
+                                                "Namibian learners licence Quiz",
+                                                "learners licence practice Namibia",
+                                                "learners licence test frans",
+                                            ],
+                                            ogImage = 'https://isqkzbwoiunnqsltbfpa.supabase.co/storage/v1/object/public/WebsiteLogo/logo.ico?t=2024-07-25T21%3A32%3A36.405Z',
+                                            twitterCard = 'summary_large_image', // Default to 'summary_large_image'
+                                        }: PageSEOProps): Metadata {
+
+    // Create Site Title
+    const siteTitle = 'Online Namibian Learners Licence Test';
+    const fullTitle = `${title} | ${siteTitle}`;
+
+    const defaultUrl = "https://namibianlearnerstest.frans-nekongo.com/"
+        ? `https://namibianlearnerstest.frans-nekongo.com/`
+        : "http://localhost:3000";
+
+    return {
+        generator: 'Next.js',
+        applicationName: 'Namibian Learners Licence Test App',
+        authors: [{name: 'Frans Nekongo', url: 'https://frans-nekongo.com/'}],
+        metadataBase: new URL(defaultUrl),
+        title: fullTitle,
+        description,
+        keywords: keywords.join(', '),
+        openGraph: {
+            title: fullTitle,
+            description,
+            type: ogType,
+            url: canonicalUrl,
+            images: [
+                {
+                    url: ogImage,
+                },
+            ],
+        },
+        twitter: {
+            card: twitterCard,
+            title: fullTitle,
+            description,
+            images: [ogImage],
+        },
+        alternates: {
+            canonical: canonicalUrl,
+        },
+        // App-specific metadata
+        appleWebApp: {
+            capable: true,
+            title: fullTitle,
+            statusBarStyle: 'black-translucent',
+        },
+        // Robots directives
+        robots: {
+            index: true,
+            follow: true,
+            nocache: true,
+            googleBot: {
+                index: true,
+                follow: true,
+            },
+        },
+    };
+}
