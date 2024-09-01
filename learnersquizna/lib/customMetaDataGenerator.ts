fccaac7e27c7a80474e39af9306a721ef4d58461
frans nekongo <FRANSINATOR2002@GMAIL.COM>
2024-09-01 02:15:16 +0200

    Update customMetaDataGenerator.ts

--- learnersquizna/lib/customMetaDataGenerator.ts
+++ learnersquizna/lib/customMetaDataGenerator.ts
@@ -35,13 +35,21 @@ export function customMetaDataGenerator({
                                                 "Namibian learners licence Quiz",
                                                 "learners licence practice Namibia",
                                                 "learners licence test frans",
+                                                "Practice learners online",
+                                                "Practice learners licence practice Namibia",
+                                                "Practice learners licence Namibia",
+                                                "Practice Namibian learners licence test",
+                                                "Practice Namibian learners licence test online",
+                                                "learners licence online",
+                                                "Namibian learners licence test online",
+                                                "Namibian learners licence online",
                                             ],
                                             ogImage = 'https://isqkzbwoiunnqsltbfpa.supabase.co/storage/v1/object/public/WebsiteLogo/logo.ico?t=2024-07-25T21%3A32%3A36.405Z',
                                             twitterCard = 'summary_large_image', // Default to 'summary_large_image'
                                         }: PageSEOProps): Metadata {
 
     // Create Site Title
-    const siteTitle = 'Online Namibian Learners Licence Test';
+    const siteTitle = 'Online';
     const fullTitle = `${title} | ${siteTitle}`;
 
     const defaultUrl = "https://namibianlearnerstest.frans-nekongo.com/"
